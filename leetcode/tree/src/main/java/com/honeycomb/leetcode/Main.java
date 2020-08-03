6e6781b2cc4340f4d978eb634fed7cdf5d1ff9e1
maoliang <maoliang@ifeng.com>
2020-08-03 18:16:39 +0800

    树

---
+++ leetcode/tree/src/main/java/com/honeycomb/leetcode/Main.java
@@ -0,0 +1,19 @@
+package com.honeycomb.leetcode;
+
+import com.honeycomb.leetcode.entity.TreeNode;
+
+import java.util.List;
+
+/**
+ * @author maoliang
+ * @version 1.0.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 4);
+        List<Integer> list1 = List.of(1, 3, 5);
+        TreeNode root = new TreeNode(-1, 0);
+
+    }
+}
