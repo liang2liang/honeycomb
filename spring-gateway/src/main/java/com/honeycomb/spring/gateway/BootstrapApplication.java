45bf9e7bdca8a5359183c417c464535f5b19080a
maoliang <maoliang@ifeng.com>
2020-10-28 18:03:35 +0800

    增加gateway demo

---
+++ spring-gateway/src/main/java/com/honeycomb/spring/gateway/BootstrapApplication.java
@@ -0,0 +1,15 @@
+package com.honeycomb.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maoliang
+ */
+@SpringBootApplication
+public class BootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapApplication.class, args);
+    }
+}
